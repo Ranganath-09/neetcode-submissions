b6afb792978654741356885e2421b0987b7340c1
Ranganath Reddy <praneethr841@gmail.com>
2026-08-16 15:31:56 +0530

    Add: concatenation-of-array - submission-4

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-4.cpp
@@ -0,0 +1,22 @@
+/*
+Let us try to read other codes ,learn and modify 
+In c++ for vectors way   .insert()   
+Multiple Copies: ans.insert(pos, n, val) inserts n copies of val before pos.
+
+n is  
+*/
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+    vector<int> ans;
+     ans.reserve(2*nums.size());
+   
+     ans=nums;
+     
+     ans.insert(ans.end(),nums.begin(),nums.end());
+    
+
+
+        return  ans;
+    }
+};
